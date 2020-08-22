8f38c303b4229d21b6890fa3cc15d698bb356a0f
abdurrahmanekr <abdurrahmaneker58@gmail.com>
2020-08-22 15:58:35 +0300

    anlık statement değerleri yansıtılması yazıldı

--- admin/public/js/front.js
+++ admin/public/js/front.js
@@ -37,6 +37,51 @@ $(function () {
         return false;
     });
 
+    const conf = {
+        endpoint: 'https://sample-lrs-paiseze.lrs.io/xapi/',
+        auth: 'Basic ' + toBase64('username:password'),
+    };
+
+    ADL.XAPIWrapper.changeConfig(conf);
+
+    window.fetchStataments = function () {
+        const parameters = ADL.XAPIWrapper.searchParams();
+
+        parameters['verb'] = 'http://adlnet.gov/expapi/verbs/initialized';
+        parameters['activity'] = 'https://www.coursera.org/learn/machine-learning/1';
+
+        const queryData = ADL.XAPIWrapper.getStatements(parameters);
+
+        if (queryData.statements && queryData.statements.length > 1) {
+            let list = [];
+
+            queryData.statements.forEach(function(x, i) {
+                list.push(`
+                    <div class="d-flex justify-content-between align-items-start align-items-sm-center mb-4 flex-column flex-sm-row">
+                      <div class="left d-flex align-items-center">
+                        <div class="text">
+                          <h6 class="mb-0 d-flex align-items-center"> <span>${x.actor && x.actor.name || 'Bilinmeyen Birisi'}</span><span class="dot dot-sm ml-2 mr-2 bg-violet"></span> ${x.object.definition.description['en-US']}</h6><small class="text-gray">${new Date(x.timestamp).toLocaleString()}</small>
+                        </div>
+                      </div>
+                      <div class="right ml-5 ml-sm-0 pl-3 pl-sm-0 text-violet">
+                        <h5>${x.verb.display['en-US']}</h5>
+                      </div>
+                    </div>
+                `.trim());
+            });
+
+            let eventList = document.getElementById('event-list');
+
+            if (eventList) {
+                eventList.innerHTML = list.join('\n');
+            }
+        }
+
+        setTimeout(window.fetchStataments, 1000);
+    };
+
+    fetchStataments();
+
 });
 
 
